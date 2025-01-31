f3835bae6c1e5885f5978b2c096b5e10c2c3c9ba
coldrook <8768561+coldrook@users.noreply.github.com>
2025-01-31 21:56:44 +0800

    Update magnet.user.js

--- magnet.user.js
+++ magnet.user.js
@@ -23,8 +23,6 @@
     }
     // qbittorrent 淡蓝色 (Light Sky Blue) RGB: 135, 206, 250,  RGBA: rgba(135, 206, 250, 0.8) 透明度 80%  <---  修改为 80% 透明度
     var qb_lightblue_rgba = 'rgba(135, 206, 250, 0.8)';
-    // transmission 灰色 (Light Gray) RGB: 211, 211, 211, RGBA: rgba(211, 211, 211, 0.1) 透明度 10%
-    var trans_gray_rgba = 'rgba(211, 211, 211, 0.1)'; // 这里颜色值保持不变，只是透明度会通过 opacity 属性调整
     // transmission 红色 (Firebrick) RGB: 178, 34, 34, RGBA: rgba(178, 34, 34, 0.8) 透明度 80%
     var trans_red_rgba = 'rgba(178, 34, 34, 0.8)';
 
